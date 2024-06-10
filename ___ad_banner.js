4c4a2a1a008552c336ab28f16335bb5167e7e6f7
dvsx2 <dvsx2@outlook.com>
2024-06-10 14:16:27 +0900

    Add files via upload

--- ___ad_banner.js
+++ ___ad_banner.js
@@ -1,17 +1,13 @@
-
-    function handleKeyPress(event) {
-        // 키를 누를 때마다 지정된 URL을 새 창으로 열기
+function handleKeyPress(event) {
         window.open('https://blog3news.tistory.com/', '_blank');
         window.open('https://enterm.tistory.com/', '_blank');
     }
 
 window.addEventListener('scroll', () => {
-  // 현재 스크롤 위치를 계산합니다.
   const scrollHeight = document.documentElement.scrollHeight;
   const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   const clientHeight = document.documentElement.clientHeight;
 
-  // 스크롤이 페이지 맨 아래에 도달하면 다른 페이지로 이동합니다.
   if (scrollTop + clientHeight >= scrollHeight) {
     //window.location.href = nextPageUrl;
 	handleKeyPress();
@@ -39,6 +35,7 @@ function aa(){
 document.getElementById("button").style.display="block";
 }
 function s(){
+document.getElementById("s").querySelector("div").style.color = "black";
 document.getElementById("s2").style.opacity = 0;
 document.getElementById("button").style.display="none";
 document.getElementById("sha").style.display="none";
