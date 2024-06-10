57e95b61f166b53222dc727afb7ff55f38357bef
dvsx2 <dvsx2@outlook.com>
2024-06-10 14:26:42 +0900

    Add files via upload

--- ___ad_banner.js
+++ ___ad_banner.js
@@ -1,4 +1,4 @@
-function handleKeyPress(event) {
+	function handleKeyPress(event) {
         window.open('https://blog3news.tistory.com/', '_blank');
         window.open('https://enterm.tistory.com/', '_blank');
     }
@@ -21,11 +21,11 @@ history.pushState(null,null,""),window.onpopstate=function(a){a&&(history.back()
 document.getElementById("s2").style.transition = "opacity 5s";
 document.getElementById('container').style.boxShadow = 'none';
 function ss(){
-setTimeout("s()", 6999);
+setTimeout("s()", 1999);
 setTimeout("h()", 9999);
 }
 function pcss(){
-setTimeout("s()", 6999);
+setTimeout("s()", 1999);
 setTimeout("h()", 9999);
 }
 function a(){
@@ -35,7 +35,10 @@ function aa(){
 document.getElementById("button").style.display="block";
 }
 function s(){
-document.getElementById("s").querySelector("div").style.color = "black";
+var allElements = document.querySelectorAll("*");
+for (var i = 0; i < allElements.length; i++) {
+    allElements[i].style.color = "black";
+}
 document.getElementById("s2").style.opacity = 0;
 document.getElementById("button").style.display="none";
 document.getElementById("sha").style.display="none";
